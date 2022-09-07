e157fe056643e4abe9b20435091f5416d0bec62f
Yuriy Novikov <7413856@mail.ru>
2022-09-08 00:55:54 +0300

    задача 4

--- Type_2.py
+++ Type_2.py
@@ -1,12 +1,14 @@
 # 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 #     *Пример:*
 #     - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-print('введите число N: ')
-n = int(input())
+n = int(input('введите число N: '))
 print(f'набор произведений чисел от 1 до {n}: ')
 factorial = 1
 i = 1
+result=[]
 while i <= n:
     factorial = factorial*i
+    result.append(factorial)
     i += 1
-    print(f'{factorial}')
+   
+print(f'{result}')
