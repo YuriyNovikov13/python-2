647518fd4172e2a6cc937bb72f3dd3d6fe036d72
Yuriy Novikov <7413856@mail.ru>
2022-09-08 11:06:05 +0300

    задача 5

--- Type_5.py
+++ Type_5.py
@@ -1,28 +1,11 @@
 # 5. Реализуйте алгоритм перемешивания списка.
+
 n = list(input('введите список: '))
 print(n)
-l=int(len(n))
-
+l = int(len(n))
+import random
 for i in range(l):
-    j =int(n.random.randint(0, l-1))
-    element=n.pop(j)
+    j = random.randint(0, l-1)
+    element = n.pop(j)
     n.append(element)
 print(n)
- # Assign array
-# arr = [1, 2, 3, 4, 5, 6]
- 
-# # Display original array
-# print("Original List: ", arr)
- 
-# # Get length of List
-# n = len(arr)
- 
-# #repeat the following for n number of times
-# for i in range(n):
-#     #select an index randomly
-#     j = random.randint(0, n-1)
-#     #delete the element at that index.
-#     element=arr.pop(j)
-#     #now append that deleted element to the list
-#     arr.append(element)
-# print("Shuffled List: ",arr)   
